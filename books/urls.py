d4f63b438964ca4b793c2811f62641e3feba7c4d
Bassam Saad <bassamsaad771@gmail.com>
2021-04-22 04:43:02 +0200

    add Book Model with CRUD operations user mysql db

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("create", views.create, name="create"),
+    path("edit/<int:id>", views.edit, name="edit"),
+    path("delete/<int:id>", views.destroy, name="destroy"),
+    path("show/<int:id>", views.show, name="show"),
+]
